c7b6e32980367d4ccbcc6a09646972a49ba6af47
Mini <evan.saleck@gmail.com>
2024-03-13 13:22:38 +0100

    [MODIF] function.js

--- JS/Fonctions/function.js
+++ JS/Fonctions/function.js
@@ -2,12 +2,12 @@
  * Fonction qui lit les données et importe les données dans les classes Pokemons et les stocke dans la variable de classe all_pokemons
  */
 function import_pokemon() {
-    pokemon.forEach(poke => {
+    pokemon.forEach(pokemon => {
         try {
-            if (poke.form === "Normal") {
-                var newPokemon = new Pokemon(poke.pokemon_name, poke.form, poke.base_attack, poke.base_defense, poke.base_stamina);
-                // Utilisation de Object.defineProperty pour ajouter le nouveau Pokémon à la variable de classe all_pokemon
-                Object.defineProperty(Pokemon.all_pokemon, poke.pokemon_id, { value: newPokemon, writable: true, enumerable: true, configurable: true });
+            if (pokemon.form === "Normal") {
+                var Poke = new Pokemon(pokemon.pokemon_name, pokemon.form, pokemon.base_attack, pokemon.base_defense, pokemon.base_stamina);
+                // Ajout du Pokémon dans la liste de tous les pokémons 
+                Pokemon.all_pokemon[pokemon.pokemon_id] = Poke;
             }
         } catch (error) {
             console.error("Une erreur est survenue lors de l'importation du Pokémon :", error);
